aee09af4e0858d6888ee8d34e8622a1625041a9f
agentpro2307-cmd <agent.pro2307@gmail.com>
2026-02-05 15:28:17 +0100

    Update PropertyList.tsx

--- PropertyList.tsx
+++ PropertyList.tsx
@@ -23,7 +23,7 @@ const MOCK_PROPERTIES: Property[] = [
     baths: 0,
     sqft: 160,
     type: "Chalet",
-    imageUrl: "/images/escarnot_00013.jpeg",
+    imageUrl: "/images/escarnot_00013.JPG",
     url: "https://www.leboncoin.fr/ad/ventes_immobilieres/3079918529",
   },
 
