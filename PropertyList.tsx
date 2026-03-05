205988e930ace41447925c737187e1a30e644f77
agentpro2307-cmd <agent.pro2307@gmail.com>
2026-03-05 01:00:23 +0100

    Update PropertyList.tsx

--- PropertyList.tsx
+++ PropertyList.tsx
@@ -100,7 +100,7 @@ const MOCK_PROPERTIES: Property[] = [
     sqft: 228,
     type: "Villa",
     imageUrl: "/images/majetniak.png",
-    url: "https://www.leboncoin.fr/ad/ventes_immobilieres/3079918531",
+    url: "https://www.bellesdemeures.com/annonces/vente/tt-2-tb-2-pl-515/252813665/?serp_view=list&search=distributionTypes%3DBuy%26estateTypes%3DHouse%2CApartment%26locations%3DAD08FR148&sort_order_ab_test=sort_order_reference&feature_aiEnrichment=false#ln=classified_search_results&m=classified_search_results_classified_classified_detail_L",
      badges: ["Coup de coeur"],
   },
   {
