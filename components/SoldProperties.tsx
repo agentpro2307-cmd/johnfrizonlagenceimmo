223dc2bdc8012d265e82247b41e3b50ba85e23a7
agentpro2307-cmd <agent.pro2307@gmail.com>
2026-04-03 16:53:55 +0200

    Update SoldProperties.tsx

--- components/SoldProperties.tsx
+++ components/SoldProperties.tsx
@@ -134,7 +134,7 @@ const SOLD_PROPERTIES: SoldProperty[] = [
     soldInDays: 45,
     exclusivite: false,
     images: [
-      "/public/images/guyot.png",
+      "/images/vendus/guyot.png",
       "/images/vendus/VA24027original.jpg",
       "/images/vendus/VA240212original.jpg",
       "/images/vendus/VA240213original.jpg",
